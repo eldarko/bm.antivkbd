3604486730e28d012ba4f5bba3413430237ab46e
Eldar Kononov <ekononov@gmail.com>
2011-03-02 12:10:20 +0300

    Google Analytics block added

--- bm.antivkbd.user.js
+++ bm.antivkbd.user.js
@@ -60,3 +60,14 @@ unsafeWindow.doOnSubmit = function(){
 	default_handler();
 }
 
+// Блок Google Analytics. Позволяет понять, сколько людей вообще пользуются скриптом.
+// При желании, его можно удалить, но я был бы благодарен, если вы его оставите.
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-21768398-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
